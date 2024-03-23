ecd77288fe6da2d380743eb75ae97e483dbd1c7f
Kevin Remisoski <kremisoski@gmail.com>
2024-03-23 17:59:57 -0400

    updated starting weapon to include default behavior

--- code/game/g_client.c
+++ code/game/g_client.c
@@ -1231,27 +1231,17 @@ void ClientSpawn(gentity_t *ent) {
 
 		// select the highest weapon number available, after any
 		// spawn given items have fired
-		//client->ps.weapon = 1;
+		client->ps.weapon = 1;
 		if (!g_startingWeapon.integer) {
-			client->ps.weapon = 2;
+			for ( i = WP_NUM_WEAPONS - 1 ; i > 0 ; i-- ) {
+				if ( client->ps.stats[STAT_WEAPONS] & ( 1 << i ) ) {
+					client->ps.weapon = i;
+					break;
+				}
+			}
 		} else {
 			client->ps.weapon = g_startingWeapon.integer;
 		}
-		/* for ( i = WP_NUM_WEAPONS - 1 ; i > 0 ; i-- ) {
-			if ( client->ps.stats[STAT_WEAPONS] & ( 1 << i ) ) {
-				client->ps.weapon = i;
-				break;
-			}
-
-			if (g_startArmor.integer > 0) {
-				client->ps.stats[STAT_ARMOR] = g_startArmor.integer;
-				if (client->ps.stats[STAT_ARMOR] > client->ps.stats[STAT_MAX_HEALTH] * 2) {
-					client->ps.stats[STAT_ARMOR] = client->ps.stats[STAT_MAX_HEALTH] * 2;
-				}
-			}
-
-
-		} */
 		if (g_startArmor.integer > 0) {
 			client->ps.stats[STAT_ARMOR] = g_startArmor.integer;
 			if (client->ps.stats[STAT_ARMOR] > client->ps.stats[STAT_MAX_HEALTH] * 2) {
