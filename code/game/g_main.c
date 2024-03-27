59269430c01b59425042f45f1edfb5a398af32a5
Kevin Remisoski <kremisoski@gmail.com>
2024-03-27 05:36:50 -0400

    map and asset updates

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -205,10 +205,10 @@ void G_RemapTeamShaders( void ) {
 #ifdef MISSIONPACK
 	char string[1024];
 	float f = level.time * 0.001;
-	Com_sprintf( string, sizeof(string), "team_icon/%s_red", g_redteam.string );
+	Com_sprintf( string, sizeof(string), "team_icon/%s_red", "Stroggs" ); //fix g_redteam.string
 	AddRemap("textures/ctf2/redteam01", string, f); 
 	AddRemap("textures/ctf2/redteam02", string, f); 
-	Com_sprintf( string, sizeof(string), "team_icon/%s_blue", g_blueteam.string );
+	Com_sprintf( string, sizeof(string), "team_icon/%s_blue", "Pagans" ); //fix g_blueteam.string
 	AddRemap("textures/ctf2/blueteam01", string, f); 
 	AddRemap("textures/ctf2/blueteam02", string, f); 
 	trap_SetConfigstring(CS_SHADERSTATE, BuildShaderStateConfig());
